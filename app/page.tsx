97a1a5a7d221d74f54c7806e7f8498476a30fbf1
JadirJunior <jadirjunior8@gmail.com>
2024-09-17 17:37:03 -0300

    feat: add Header components and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
-import Image from "next/image";
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/Header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <Button>Test</Button>
-  );
+    <div>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jadir!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-8">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
