a56187968bf8735d1e3684a935b73d3ddd616d7e
JadirJunior <jadirjunior8@gmail.com>
2024-09-30 17:07:22 +0100

    feat: ensure barbershop services are shown on barbershop page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
-import BookingItem from "./_components/BookingItem"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
