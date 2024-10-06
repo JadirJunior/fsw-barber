8b8d6bb07b6db497d48df93935f387fb0cbad217
JadirJunior <jadirjunior8@gmail.com>
2024-10-06 15:24:04 +0100

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,11 @@
+import { format } from "date-fns"
+import { Card, CardContent } from "./ui/card"
+import { ptBR } from "date-fns/locale"
+import React from "react"
+
+// TODO: Componente de resumo do agendamento
+const BookingSummary = () => {
+  return <></>
+}
+
+export default BookingSummary
